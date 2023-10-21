1ede7d4cea96c7ab75464b8b3c9d7435e3e50e2d
thomas <thopan.sakti@gmail.com>
2023-10-22 00:46:17 +0700

    Creating packages

---
+++ src/belajar/programming/data/Product.java
@@ -0,0 +1,11 @@
+package belajar.programming.data;
+
+public class Product {
+    String name;
+    Integer price;
+
+    Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+}

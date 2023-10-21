baf27ee8888e5bbd4fcc30ca317702b58568d802
thomas <thopan.sakti@gmail.com>
2023-10-21 19:08:12 +0700

    Create class constructors

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,22 +1,20 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person();
-        Person person2 = new Person();
-        Person person3;
-        person3 = new Person();
+        var person1 = new Person("Thomas", "Bandung");
+//        Person person2 = new Person();
+//        Person person3;
+//        person3 = new Person();
 
         System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
 
-        person1.name = "Thomas Panji";
-        person1.address = "Bandung";
+//        person1.name = "Thomas Panji";
+//        person1.address = "Bandung";
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
 
         person1.sayHello("Budi");
-        person3.sayHello("Budi");
+//        person3.sayHello("Budi");
     }
 }

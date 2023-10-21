8353b7ffeeb7c4f4537fa65f21c13bf99a3404a8
thomas <thopan.sakti@gmail.com>
2023-10-21 19:29:38 +0700

    Variable shadowing example and how to solve it using this keyword

--- src/Person.java
+++ src/Person.java
@@ -3,9 +3,15 @@ class Person {
     String address;
     final String country = "Indonesia";
 
-    Person(String paramName, String paramAddress) {
-        name = paramName;
-        address = paramAddress;
+//    Person(String paramName, String paramAddress) {
+//        name = paramName;
+//        address = paramAddress;
+//    }
+
+    //    mengatasi variable shadowing, pakai this
+    Person(String name, String address){
+        this.name = name;
+        this.address = address;
     }
 
     Person(String paramName) {
@@ -21,7 +27,11 @@ class Person {
 //        address = address;
 //    }
 
+//    void sayHello(String paramName) {
+//        System.out.println("Hello " + paramName + ", My name is " + name );
+//    }
+
     void sayHello(String paramName) {
-        System.out.println("Hello " + paramName + ", My name is " + name );
+        System.out.println("Hello " + paramName + ", My name is " + this.name );
     }
 }
